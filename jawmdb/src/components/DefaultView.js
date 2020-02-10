6f78de704ba94afbf72caf9e4e4c619916335f41
A-Arndt <aarnd649@mtroyal.ca>
2020-02-09 21:43:59 -0700

    Added Some grid definition and created moviefilter

--- jawmdb/src/components/DefaultView.js
+++ jawmdb/src/components/DefaultView.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Header from './Header.js';
 import  FavoritesList from './FavoritesList.js';
 import MovieList from './MovieList.js';
+import MovieFilter from './MovieFilter.js';
 
 class DefaultView extends React.Component {   
     render() {
         return (       
-            <div>
+            <div className="default-view">
                 <Header />
                 < FavoritesList favorites={ this.props.favorites } />
+                < MovieFilter />
                 < MovieList />
             </div>         
         ); 
---
+++ jawmdb/src/components/MovieFilter.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class MovieFilter extends React.Component {
+    render() {
+        return(
+            <div className="filter">
+                <h2>Movie Filters</h2>
+                <input value="" ></input>
+
+            </div>
+        );
+    }
+}
+export default MovieFilter;
