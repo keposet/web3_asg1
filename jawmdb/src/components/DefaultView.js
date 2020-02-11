10526b05249e43b0a8bf3895333e480210071107
A-Arndt <aarnd649@mtroyal.ca>
2020-02-10 21:32:47 -0700

    Movie the fetch in default view out of movie list

--- jawmdb/src/components/DefaultView.js
+++ jawmdb/src/components/DefaultView.js
@@ -5,13 +5,45 @@ import MovieList from './MovieList.js';
 import MovieFilter from './MovieFilter.js';
 
 class DefaultView extends React.Component {   
+
+    state = { loading:true, movies:[], filteredMovies:[] };
+
+    async componentDidMount() {
+        try {
+            const movieUrl = "http://www.randyconnolly.com/funwebdev/3rd/api/movie/movies-brief.php?id=ALL";
+            const response = await fetch(movieUrl);
+            const jsonData = await response.json();
+
+            jsonData.sort( function(a,b) {
+                var nameA = a.title.toUpperCase();
+                var nameB = b.title.toUpperCase();
+                if (nameA < nameB) {
+                    return -1;
+                  }
+                if (nameA > nameB) {
+                    return 1;
+                  }
+                return 0
+            } );
+            console.log(jsonData);
+            this.setState( { loading:false, movies: jsonData, filteredMovies: jsonData} );
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    filter = () => {
+        console.log("Filtering");
+    }
+
     render() {
         return (       
             <div className="default-view">
                 <Header />
                 < FavoritesList favorites={ this.props.favorites } />
-                < MovieFilter />
-                < MovieList />
+                < MovieFilter filter={this.filter} />
+                < MovieList movies={ this.state.filteredMovies } />
             </div>         
         ); 
     }
--- jawmdb/src/components/MovieList.js
+++ jawmdb/src/components/MovieList.js
@@ -2,61 +2,22 @@ import React from 'react'
 import MovieItem from './MovieItem'
 
 class MovieList extends React.Component {
-    state = { loading:true, movies:[] };
-
-    async componentDidMount() {
-        try {
-            const movieUrl = "http://www.randyconnolly.com/funwebdev/3rd/api/movie/movies-brief.php?id=ALL";
-            const response = await fetch(movieUrl);
-            const jsonData = await response.json();
-            console.log(jsonData);
-            jsonData.sort( function(a,b) {
-                var nameA = a.title.toUpperCase();
-                var nameB = b.title.toUpperCase();
-                if (nameA < nameB) {
-                    return -1;
-                  }
-                if (nameA > nameB) {
-                    return 1;
-                  }
-                return 0
-            } );
-            console.log(jsonData);
-            this.setState( { loading:false, movies: jsonData } );
-        }
-        catch (error) {
-            console.log(error);
-        }
-    }
-
-    renderList() {
-        return(
-            <div className='list'>
-                <h2>List/Match</h2>
-                <h3>Title</h3>
-                <h3>Year</h3>
-                <h3>Rating</h3>
-                <ul className='moviesList'>
-                    {this.state.movies.map( (m) => <MovieItem title={m.title} poster={m.poster} rating={m.ratings.average} year={m.release_date} key={m.id}/>)}
-                </ul>
-                
-            </div>
-        );
-    }
-
-    renderLoading() {
-        return(
-            <div className='list'>
-            Loading hahaha
-            </div>
-        );
-    }
 
     render() {
-        if( this.state.loading)
-            return this.renderLoading();
-        else
-            return this.renderList();
+        return (
+            <div className='list'>
+            <h2>List/Match</h2>
+            <h3>Title</h3>
+            <h3>Year</h3>
+            <h3>Rating</h3>
+            <div className='loading'>
+            Loading hahaha
+            </div>
+            <ul className='moviesList'>
+                {/* {this.props.movies.map( (m) => <MovieItem title={m.title} poster={m.poster} rating={m.ratings.average} year={m.release_date} key={m.id}/>)} */}
+            </ul>
+        </div>
+        );
     }
 }
 export default MovieList;
