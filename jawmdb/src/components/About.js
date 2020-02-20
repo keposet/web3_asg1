d038ce3ed84557914daadaf57e27ad54fe5852e9
A-Arndt <aarnd649@mtroyal.ca>
2020-02-19 18:12:08 -0700

    Add node-modal and basic modal pop-up for about

---
+++ jawmdb/src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = function (props) {
+    return (
+        <div>
+            App made by Jason and Austin.
+        </div>
+    );
+}
+
+export default About;
--- jawmdb/src/components/HeaderMenu.js
+++ jawmdb/src/components/HeaderMenu.js
@@ -1,9 +1,49 @@
 import React from 'react'; 
-const HeaderMenu = function (props) {   
-    return (       
-        <nav className="header-menu">           
-            <button>About</button>   
-        </nav>          
-    ); 
+import Modal from 'react-modal'
+import About from './About'
+class HeaderMenu extends React.Component {   
+    constructor(props) {
+        super(props);
+        this.state ={
+            modalIsOpen: false
+        };
+
+        this.openModal = this.openModal.bind(this);
+        this.afterOpenModal = this.afterOpenModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+    }
+
+    openModal() {
+        this.setState({modalIsOpen: true})
+    }
+
+    afterOpenModal(){
+        //
+    }
+
+    closeModal(){
+        this.setState( { modalIsOpen: false});
+    }
+
+    render() {
+        return (       
+            <nav className="header-menu">           
+                <button onClick={this.openModal}>About</button>   
+
+                <Modal
+                    isOpen={this.state.modalIsOpen}
+                    onAfterOpen={this.afterOpenModal}
+                    onRequestClose={this.closeModal}
+                    style=""
+                    contentLabel="Example Modal"
+                >
+                    <h2 >Hello</h2>
+                    <About />
+                    <button onClick={this.closeModal}>close</button>
+                    
+                </Modal>
+            </nav>          
+        ); 
+    }
 } 
 export default HeaderMenu;
