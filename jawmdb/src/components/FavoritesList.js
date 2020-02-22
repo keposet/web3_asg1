8f620a1765e882ad3e228c8a799b8ad703ac58b6
A-Arndt <aarnd649@mtroyal.ca>
2020-02-22 00:31:06 -0700

    Create a transition animation for the favorites

--- jawmdb/src/components/FavoritesList.js
+++ jawmdb/src/components/FavoritesList.js
@@ -1,5 +1,10 @@
-import React from 'react'
-import FavoriteItem from "./FavoriteItem"
+import React from 'react';
+import FavoriteItem from "./FavoriteItem";
+import { CSSTransition } from 'react-transition-group';
+import './Favorites.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDoubleRight } from "@fortawesome/free-solid-svg-icons";
+import { faAngleDoubleLeft } from "@fortawesome/free-solid-svg-icons";
 
 class FavoritesList extends React.Component {
     state = {hidden: false};
@@ -9,34 +14,37 @@ class FavoritesList extends React.Component {
         this.setState( { hidden: newHidden } );
     }
     render() {
-        if(!this.state.hidden) {
+
+        const arrow = (this.state.hidden) ? (<FontAwesomeIcon icon={faAngleDoubleRight}/>) : (<FontAwesomeIcon icon={faAngleDoubleLeft}/>);
             return (
                 <div className="favorites-bar">
                     <h3> Favorites </h3>
-                    <button onClick={this.handleHiden}>Hide</button>
-                    <ul className="favorites">
-                        {   this.props.favorites.map( (fav, ind) => 
-                                <FavoriteItem 
-                                    poster={fav.poster} 
-                                    id={fav.id} 
-                                    title={fav.title}  
-                                    removeFav={ this.props.removeFav } 
-                                    key={ind}
-                                />
-                            )
-                        }
-                    </ul>
+                    <div onClick={this.handleHiden}>{arrow}</div>
+                    
+                    <CSSTransition
+                        in={!this.state.hidden}
+                        timeout={500}
+                        classNames="display"
+                        unmountOnExit
+                        appear
+                    > 
+                        <ul className="favorites">
+                            {   this.props.favorites.map( (fav, ind) => 
+                                    <FavoriteItem 
+                                        poster={fav.poster} 
+                                        id={fav.id} 
+                                        title={fav.title}  
+                                        removeFav={ this.props.removeFav } 
+                                        key={ind}
+                                    />
+                                )
+                            }
+                        </ul>
+                    </CSSTransition>
                 </div>
     
             );
-        } else {
-            return(
-                <div className="favorites-bar">
-                    <h3> Favorites </h3>
-                    <button onClick={this.handleHiden}>Hide</button>
-                </div>
-            );
-        }
+        
 
     }
 }
