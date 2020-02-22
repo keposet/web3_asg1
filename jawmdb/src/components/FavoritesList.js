66029a1cacd7ef9683daf325864fe807cfab0176
A-Arndt <aarnd649@mtroyal.ca>
2020-02-21 17:01:46 -0700

    Hide favorites on click of hiding button.

--- jawmdb/src/components/FavoritesList.js
+++ jawmdb/src/components/FavoritesList.js
@@ -2,25 +2,42 @@ import React from 'react'
 import FavoriteItem from "./FavoriteItem"
 
 class FavoritesList extends React.Component {
-    render() {
-        return (
-            <div className="favorites-bar">
-                <h3> Favorites </h3>
-                <ul className="favorites">
-                    {   this.props.favorites.map( (fav, ind) => 
-                            <FavoriteItem 
-                                poster={fav.poster} 
-                                id={fav.id} 
-                                title={fav.title}  
-                                removeFav={ this.props.removeFav } 
-                                key={ind}
-                            />
-                        )
-                    }
-                </ul>
-            </div>
+    state = {hidden: false};
 
-        );
+    handleHiden = () =>{
+        const newHidden = !this.state.hidden;
+        this.setState( { hidden: newHidden } );
+    }
+    render() {
+        if(!this.state.hidden) {
+            return (
+                <div className="favorites-bar">
+                    <h3> Favorites </h3>
+                    <button onClick={this.handleHiden}>Hide</button>
+                    <ul className="favorites">
+                        {   this.props.favorites.map( (fav, ind) => 
+                                <FavoriteItem 
+                                    poster={fav.poster} 
+                                    id={fav.id} 
+                                    title={fav.title}  
+                                    removeFav={ this.props.removeFav } 
+                                    key={ind}
+                                />
+                            )
+                        }
+                    </ul>
+                </div>
+    
+            );
+        } else {
+            return(
+                <div className="favorites-bar">
+                    <h3> Favorites </h3>
+                    <button onClick={this.handleHiden}>Hide</button>
+                </div>
+            );
+        }
+
     }
 }
 export default FavoritesList;
