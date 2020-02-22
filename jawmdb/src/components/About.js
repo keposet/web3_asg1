4e5d4a572feb3f84c0910d349c02d8534c0f14bd
A-Arndt <aarnd649@mtroyal.ca>
2020-02-22 03:04:49 -0700

    Added credit for used technologies and guides

--- jawmdb/src/components/About.js
+++ jawmdb/src/components/About.js
@@ -10,6 +10,14 @@ const About = function (props) {
                 <li>React-Modal: <a href="https://www.npmjs.com/package/react-modal">react-modal npm</a> </li>
                 <li>Loading svg "Flowing Gradient" from: <a href="https://icons8.com/preloaders/en/free"> icons8 Preloaders</a></li>
                 <li>Hero Image <a href='https://images.unsplash.com/photo-1542204165-65bf26472b9b'>"8mm filmrolls"</a> From: Denise Jans <a href="https://unsplash.com/@dmjdenise">@dmjdenise</a></li>
+                <li>Font Awesome Icons from <a href="https://fontawesome.com/how-to-use/on-the-web/using-with/react">react-fontawesome</a> </li>
+                <li>CSSTransition Group used for <a href="http://reactcommunity.org/react-transition-group/css-transition">animations</a></li>
+            </ul>
+            <p>Code credit:</p>
+            <ul>
+                <li>React Router query sting basics from: <a href="https://tylermcginnis.com/react-router-query-strings/">Tyler McGinnis</a></li>
+                <li>React CSSTransition basics from <a href="https://blog.logrocket.com/improve-your-ui-with-react-transition-group-999fa35f7cae/"> LogRocket.com</a></li>
+                <li>CSS help: <a href="https://stackoverflow.com/questions/47881767/" >for adding commas</a> from user <a href="https://stackoverflow.com/users/5157454/ori-drori" >Ori Dori</a></li>
             </ul>
         </div>
     );
