0fcad3910f5cb60ef412f82a8606c3f525605f1e
keposet <jsn.hutson@gmail.com>
2020-02-24 15:03:31 -0700

    remove build from gitignoe, configure host string

---
+++ jawmdb/build/precache-manifest.f06063dd039e043bd495e682941bcd71.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29b69c12d54863d3d83cab5ae8f69a2",
+    "url": "/jawmdb/index.html"
+  },
+  {
+    "revision": "8433aa17ff123f172df3",
+    "url": "/jawmdb/static/css/main.9c321c22.chunk.css"
+  },
+  {
+    "revision": "e4e68ff269bac6bc0a81",
+    "url": "/jawmdb/static/js/2.41f18ed5.chunk.js"
+  },
+  {
+    "revision": "425f71bb909b45cb21829b79476852ea",
+    "url": "/jawmdb/static/js/2.41f18ed5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8433aa17ff123f172df3",
+    "url": "/jawmdb/static/js/main.3d556f30.chunk.js"
+  },
+  {
+    "revision": "417e51b393139852cc4a",
+    "url": "/jawmdb/static/js/runtime-main.8b418774.js"
+  },
+  {
+    "revision": "64099c07937ff8fac8bfa11632877e89",
+    "url": "/jawmdb/static/media/IMDb_logo.64099c07.svg"
+  },
+  {
+    "revision": "c320d72e324deae2922a0b7073570089",
+    "url": "/jawmdb/static/media/cinema.c320d72e.svg"
+  },
+  {
+    "revision": "8375fc44e158af54c1f1547010543b8f",
+    "url": "/jawmdb/static/media/loading.8375fc44.svg"
+  },
+  {
+    "revision": "4906bce946a19a848fb161e09dfcd1d2",
+    "url": "/jawmdb/static/media/tmdb.4906bce9.svg"
+  }
+]);
